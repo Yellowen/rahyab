81fbf9b0e1c393a8dff27e507e473d99e263e386
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2015-08-12 16:23:00 +0430

    base sms class created

--- lib/rahyab.rb
+++ lib/rahyab.rb
@@ -1,9 +1,43 @@
 require "rahyab/version"
+require "builder"
 
 module Rahyab
-  def initialize(url, user, password)
-    @url = url
-    @user = user
-    @password = password
+  class SMS
+
+    # Constructor of Rahyab SMS Gateway
+    def initialize(url, user, password)
+      @url = url
+      @user = user
+      @password = password
+      @company = "Rahyab"
+      @batchID = "test"
+    end
+
+    # Will send one or more sms to specified numbers
+    def send(sender, numbers, notes)
+      builder = Builder::XmlMarkup.new
+      builder.instruct! :xml, version: "1.0", encoding: "UTF-8"
+      builder.smsBatch(company: @company, batchID: @batchID)
+      builder
+    end
+
+    def send_batch(sender, numbers, text)
+    end
+
+    # Check delivery status of several sms
+    def get_delivery(sender, sms_id)
+    end
+
+    # Check delivery status of specified phone numer
+    def get_phone_delivery(sender, sms_id, phone)
+    end
+
+    # Check the credit that how many sms can be send
+    def get_cache
+    end
+
+
+    def recieve()
+    end
   end
 end
