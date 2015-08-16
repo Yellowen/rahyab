da68271e0d44296536dbb94f1cb0b1e2ac54cb4b
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2015-08-16 13:16:38 +0430

    send result modified

--- lib/rahyab.rb
+++ lib/rahyab.rb
@@ -1,6 +1,8 @@
 # coding: utf-8
 require "rahyab/version"
 require "builder"
+require 'net/http'
+require 'xml'
 
 module Rahyab
   class SMS
@@ -15,14 +17,16 @@ module Rahyab
 
     # Will send one or more sms to specified numbers
     def send(sender, numbers, text, **params)
-      identity = "#{Time.now.to_i}#{rand(1000000..9999999)}"
-      batchID = @company + identity
-      msgClass = params["flash"] ? "0" : "1"
-      dcs = text =~ /\p{Arabic}/ ? "8" : "0"
-      builder = Builder::XmlMarkup.new(:indent=>2)
+      # Create the send XMLmarkup
+      identity         = "#{Time.now.to_i}#{rand(1000000..9999999)}"
+      batchID          = @company + identity
+      msgClass         = params["flash"] ? "0" : "1"
+      dcs              = is_persian(text) ? "8" : "0"
+      binary           = is_persian(text) ? "true" : "false"
+      builder          = Builder::XmlMarkup.new(:indent=>2)
       builder.instruct! :xml, version: "1.0", encoding: "UTF-8"
       builder.smsBatch(company: @company, batchID: batchID) do |b|
-        b.sms(msgClass: msgClass, binary: "True", dcs: dcs) do |t|
+        b.sms(msgClass: msgClass, binary: binary, dcs: dcs) do |t|
           numbers.each do |number|
             t.destAddr() do |f|
               f.declare! "[CDATA[%s]]" % number
@@ -34,9 +38,17 @@ module Rahyab
           t.message() do |f|
             f.declare! "[CDATA[#{text}]]"
           end
-
         end
       end
+      out_xml = builder.target!
+      # Send XMLmarkup to Web Service
+      uri = URI.parse(@url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.basic_auth @user, @password
+      request.body = out_xml
+      response = http.request(request)
+      return parse_result(response.body)
     end
 
     def send_batch(sender, numbers, text)
@@ -59,9 +71,18 @@ module Rahyab
     end
 
 
-    def recieve()
+    private
+
+    # Check does the input contents Farsi Character
+    def is_persian(str)
+      str =~ /\p{Arabic}/
     end
 
-
+    # Parse XML and return the message from result
+    def parse_result(result)
+      source = XML::Parser.string(result)
+      content = source.parse
+      return content.find_first('message').content
+    end
   end
 end
